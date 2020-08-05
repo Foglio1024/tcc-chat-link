15d5c1ee0a0144e98fb5023dd682b357f168206d
Foglio1024 <foglio1024@gmail.com>
2020-08-05 11:37:00 +0200

    automatically disable on x64 client

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-module.exports = function TccChatLink(m)
+exports.ClientMod = class
 {
-    m.installGPK("S1UI_Chat2.gpk");
+    constructor(mod)
+    {
+        this.install = function(installer)
+        {
+            if(mod.clientInterface.arch == 'x64') return;
+            mod.warn("This mod will be disabled without showing any warning after 64-bit client patch hits. It will be automatically re-enabled when gpk mods will be supported again an update is released.");
+            installer.gpk("S1UI_Chat2.gpk");
+        };
+    }
 }

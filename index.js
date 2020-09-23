b62e57ef2bff06fb993ad2bd66e7c43e6d088eb8
Foglio1024 <foglio1024@gmail.com>
2020-09-23 21:20:39 +0200

    x64 update

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ exports.ClientMod = class
     {
         this.install = function(installer)
         {
-            if(mod.clientInterface.info.arch === 'x64') return;
-            mod.warn("This mod will be disabled without showing any warning after 64-bit client patch hits. It will be automatically re-enabled when gpk mods will be supported again an update is released.");
             installer.gpk("S1UI_Chat2.gpk");
         };
     }

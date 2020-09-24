d37321a914fa533b8185df0aff82ada95cfde5fe
Foglio1024 <foglio1024@gmail.com>
2020-09-24 13:03:59 +0200

    disable mod as it's merged in TCC main module

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ exports.ClientMod = class
 {
     constructor(mod)
     {
+        mod.log("This mod has been merged into TCC main module and can be uninstalled.");
+        return;
         this.install = function(installer)
         {
             installer.gpk("S1UI_Chat2.gpk");
